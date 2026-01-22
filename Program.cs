ffc94ca5e39c47713d892382aed47c18df554898
Lene <Lene-tl@hotmail.com>
2026-01-22 14:45:00 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using FirstAPI.data;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Hosting;
+
+namespace FirstAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = "Data Source=app.db";
+            builder.Services.AddSingleton(new Character(connectionString));
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            //---------------- (control * = ta vekk //)
+
+
+            //curl -X POST "https://localhost:7128/health/heal?id=1&newHealth=100"
+            //app.MapPost("/health/heal", (int id, int newHealth, Character player) =>
+            //{
+            //    var result = player.UpdateHealth(id, newHealth);
+            //    return result;
+            //});
+
+            //curl -X POST "https://localhost:7128/health/heal?id=1"
+            app.MapPost("/health/heal", (int id, Character player) =>
+            {
+                var result = player.UpdateHealth(id, GetRandomNumber());
+                return result;
+            });
+
+
+
+            //app.MapPost("/health/damage", (int) =>
+            //{
+
+            //})
+
+            //curl "https://localhost:7128/health?id=1"
+            app.MapGet("/health", (int id, Character player) =>
+            {
+                var result = player.GetHealth(id);
+                return result;
+            });
+
+            //curl "https://localhost:7128/name?id=1"
+            app.MapGet("/name", (int id, Character player) =>
+            {
+                var result = player.GetName(id);
+                return result;
+            });
+
+            app.Run();
+        }
+
+        public static int GetRandomNumber()
+        {
+            Random rand = new Random();
+            int result = rand.Next(5,15);
+            return result;
+        }
+    }
+}
---
+++ data/Character.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace FirstAPI.data
+{
+    public class Character
+    {
+        private readonly string _connectionString;
+
+        public Character(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int GetHealth(int id)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT Health FROM Character WHERE Id = $id";
+            command.Parameters.AddWithValue("$id", id);
+
+            var result = command.ExecuteScalar();
+            connection.Close();
+
+            return Convert.ToInt32(result);
+        }
+
+        public int UpdateHealth(int id, int healedValue)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+            var command = connection.CreateCommand();
+
+            command.CommandText = "UPDATE Character SET Health = $value WHERE Id = $id";
+            command.Parameters.AddWithValue("$id", id);
+            command.Parameters.AddWithValue("$value", healedValue + GetHealth(id));
+            return command.ExecuteNonQuery();
+        }
+
+        public string GetName(int id)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT Name FROM Character WHERE Id = $id";
+            command.Parameters.AddWithValue("$id", id);
+
+            var result = command.ExecuteScalar();
+            connection.Close();
+
+            return Convert.ToString(result);
+        }
+        //legge til et endepunkt som henter Navn - fixed
+        //legge til random heal
+        //legge til dmg
+    }
+}
